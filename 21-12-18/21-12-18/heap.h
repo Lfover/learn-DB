3b2b1a78348743376c691636cfb07eaeb9e9b0ae
LMR <3524613297@qq.com>
2021-12-18 22:53:51 +0800

    堆的操作（class)

---
+++ 21-12-18/21-12-18/heap.h
@@ -0,0 +1,90 @@
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <Windows.h>
+#include <string.h>
+#include <memory.h>
+#define HEAP_SIZE 20
+typedef int HeapElemType;
+
+typedef struct MinHeap
+{
+	HeapElemType *base;
+	size_t capacity;
+	size_t size;
+}MinHeap;
+
+void HeapInit(MinHeap *php,int sz=HEAP_SIZE);
+void HeapInsert(MinHeap *php, HeapElemType x);
+void HeapDelete(MinHeap *php);//删除只能删除堆顶
+
+void HeapShow(MinHeap *php);
+void HeapShiftUp(MinHeap *php, int start);
+void HeapShiftDown(MinHeap *php, int start);
+////////////////////////////////////////////
+//交换
+void Swap(HeapElemType *t1, HeapElemType *t2){
+	HeapElemType temp = *t1;
+	*t1 = *t2;
+	*t2 = temp;
+}
+
+//初始化
+void HeapInit(MinHeap *php,int sz)
+{
+	assert(php);
+	php->capacity = sz > HEAP_SIZE ? sz : HEAP_SIZE;
+	php->base = (HeapElemType*)malloc(sizeof(HeapElemType)*php->capacity);
+	memset(php->base, 0, sizeof(HeapElemType)*php->capacity);
+	php->size = 0;
+}
+//插入
+void HeapInsert(MinHeap *php, HeapElemType x)
+{
+	if (php->size > php->capacity){
+		printf("堆空间已满");
+		return;
+	}
+	php->base[php->size] = x;
+	HeapShiftUp(php,php->size);
+	php->size++;
+}
+//删除
+void HeapDelete(MinHeap *php, HeapElemType key)
+{
+
+}
+//显示
+void HeapShow(MinHeap *php)
+{
+	assert(php);
+	for (int i = 0; i < php->size; i++){
+		printf("%d", php->base[i]);
+	}
+}
+//向上调整
+void HeapShiftUp(MinHeap *php, int start)
+{
+	//i=parent,j=child
+	int j = start;
+	int i = (j - 1) / 2;
+	while (j >= 0){
+		if (php->base[j] < php->base[i]){
+			Swap(&php->base[j], &php->base[i]);
+		}
+		else{
+			break;
+		}
+	}
+}
+//向下调整
+void HeapShiftDown(MinHeap *php, int start)
+{
+
+}
+#endif//_HEAP_H_
---
+++ 21-12-18/21-12-18/main.cpp
@@ -0,0 +1,14 @@
+#include "heap.h"
+
+void main()
+{
+	int arr[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	MinHeap mhp;
+	HeapInit(&mhp);
+	for (int i = 0; i < n; i++){
+		HeapInsert(&mhp, arr[i]);
+	}
+	HeapShow(&mhp);
+	system("pause");
+}
